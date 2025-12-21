ffd39d16943435c1fd196c326d5f013c9483aeb1
Matt Kagie <mjkagie@gmail.com>
2025-12-20 18:25:34 -0800

    Parts 1 and 2 now work, because I switched to doing it with geo -- I really should learn more about geo, that was speeeeeed

--- day-nine/src/main.rs
+++ day-nine/src/main.rs
@@ -1,8 +1,8 @@
 //! Command line executable for running part one and part two
-use std::{cmp::Ordering, time::Instant};
+use std::time::Instant;
 
 use clap::Parser;
-use nalgebra::DMatrix;
+use geo::{Contains as _, Coord, LineString, Polygon, Rect};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -57,70 +57,6 @@ impl Location {
         (((self.x as isize - other.x as isize).abs() + 1)
             * ((self.y as isize - other.y as isize).abs() + 1)) as usize
     }
-
-    pub fn get_range(&self, other: &Location) -> Range {
-        if self.x == other.x {
-            Range {
-                direction: Direction::Y,
-                start: self.y.min(other.y),
-                end: self.y.max(other.y),
-            }
-        } else if self.y == other.y {
-            Range {
-                direction: Direction::X,
-                start: self.x.min(other.x),
-                end: self.x.max(other.x),
-            }
-        } else {
-            panic!("This doesn't make sense")
-        }
-    }
-
-    /// Return bottom left and top right
-    pub fn get_corners(&self, other: &Location) -> (Location, Location) {
-        // bottom left is the min x and the max y
-        let min_x = self.x.min(other.x);
-        let max_y = self.y.max(other.y);
-        let bottom_left = Location { x: min_x, y: max_y };
-        // Top right is max x and min y
-        let max_x = self.x.max(other.x);
-        let min_y = self.y.min(other.y);
-        let top_right = Location { x: max_x, y: min_y };
-        (bottom_left, top_right)
-    }
-}
-impl PartialOrd for Location {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        // We will define less than as is more bottom left -- so x is <= and y is >=
-        if self.x <= other.x && self.y >= other.y {
-            Some(Ordering::Less)
-        }
-        // We will define greater than as is more upper right, so x >= and y is <=
-        else if self.x >= other.x && self.y <= other.y {
-            Some(Ordering::Greater)
-        } else if self.x == other.x && self.y == other.y {
-            Some(Ordering::Equal)
-        } else {
-            None
-        }
-    }
-}
-
-/// Range for sweeping
-#[derive(Debug, Clone)]
-struct Range {
-    direction: Direction,
-    // Inclusive
-    start: usize,
-    // Inclusive
-    end: usize,
-}
-
-/// direction
-#[derive(Debug, Clone)]
-enum Direction {
-    X,
-    Y,
 }
 
 /// Driver
@@ -147,93 +83,7 @@ impl Driver {
     }
 
     pub fn part_two(&self) -> usize {
-        // Create the board
-        let largest_x_value = self
-            .red_tiles
-            .iter()
-            .fold(0_usize, |max_value, current_value| {
-                max_value.max(current_value.x)
-            });
-        let largest_y_value = self
-            .red_tiles
-            .iter()
-            .fold(0_usize, |max_value, current_value| {
-                max_value.max(current_value.y)
-            });
-        // 0 -- open, 1 -- red, 2 -- green
-        let mut board = DMatrix::from_element(largest_y_value + 1, largest_x_value + 1, 0_u8);
-        for idx0 in 0..self.red_tiles.len() {
-            let red_tile = &self.red_tiles[idx0];
-            // Mark the current as red
-            *board.get_mut((red_tile.y, red_tile.x)).unwrap() = 1;
-            if idx0 > 0 {
-                let prev_red_tile = &self.red_tiles[idx0 - 1];
-                let range_that_is_green = red_tile.get_range(prev_red_tile);
-                match range_that_is_green.direction {
-                    Direction::X => {
-                        for x in range_that_is_green.start..=range_that_is_green.end {
-                            if board[(red_tile.y, x)] == 0 {
-                                *board.get_mut((red_tile.y, x)).unwrap() = 2;
-                            }
-                        }
-                    }
-                    Direction::Y => {
-                        for y in range_that_is_green.start..=range_that_is_green.end {
-                            if board[(y, red_tile.x)] == 0 {
-                                *board.get_mut((y, red_tile.x)).unwrap() = 2;
-                            }
-                        }
-                    }
-                }
-            }
-            if idx0 == self.red_tiles.len() - 1 {
-                let prev_red_tile = &self.red_tiles[0];
-                let range_that_is_green = red_tile.get_range(prev_red_tile);
-                match range_that_is_green.direction {
-                    Direction::X => {
-                        for x in range_that_is_green.start..=range_that_is_green.end {
-                            if board[(red_tile.y, x)] == 0 {
-                                *board.get_mut((red_tile.y, x)).unwrap() = 2;
-                            }
-                        }
-                    }
-                    Direction::Y => {
-                        for y in range_that_is_green.start..=range_that_is_green.end {
-                            if board[(y, red_tile.x)] == 0 {
-                                *board.get_mut((y, red_tile.x)).unwrap() = 2;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        println!("Initial board -- {}x{}", board.nrows(), board.ncols());
-        // Now, fill in the board
-        for idx_r in 0..board.nrows() {
-            // We need to find the first non-zero and the last non-zero
-            let mut first_non_zero = board.ncols();
-            let mut last_non_zero = 0_usize;
-            for idx_c in 0..board.ncols() {
-                if board[(idx_r, idx_c)] != 0 {
-                    first_non_zero = first_non_zero.min(idx_c);
-                    last_non_zero = last_non_zero.max(idx_c);
-                }
-            }
-            // Go through again and set
-            for idx_c in 0..board.ncols() {
-                if idx_c > first_non_zero && idx_c < last_non_zero && board[(idx_r, idx_c)] == 0 {
-                    *board.get_mut((idx_r, idx_c)).unwrap() = 2;
-                }
-            }
-            if idx_r % 100 == 0 {
-                println!(
-                    "Completed {idx_r} rows of {} -- {:0.2}%",
-                    board.nrows(),
-                    idx_r as f32 / board.nrows() as f32 * 100.0
-                );
-            }
-        }
-        println!("Filled in the board");
+        let board = Board::new(&self.red_tiles);
         let mut areas = Vec::new();
         for idx0 in 0..self.red_tiles.len() - 1 {
             let tile0 = &self.red_tiles[idx0];
@@ -246,25 +96,16 @@ impl Driver {
                     tile1: tile1.clone(),
                 });
             }
-            println!(
-                "Completed {idx0} tiles of {} -- {:0.2}%",
-                self.red_tiles.len(),
-                idx0 as f32 / self.red_tiles.len() as f32 * 100.0
-            );
         }
-        println!("Completed areas");
         // Now, we need to sort the areas and then iterate until we find one that is valid
         areas.sort_by_key(|val| val.area);
         areas.reverse();
-        let mut previously_invalidated_locations = Vec::new();
         for area in areas {
-            if area.validate(&board, &previously_invalidated_locations) {
+            if board.contains(&area) {
                 return area.area;
-            } else {
-                previously_invalidated_locations.push(area);
             }
         }
-        0
+        panic!("All are impossible");
     }
 }
 
@@ -275,44 +116,35 @@ struct AreaResults {
     tile0: Location,
     tile1: Location,
 }
-impl AreaResults {
-    pub fn validate(
-        &self,
-        board: &DMatrix<u8>,
-        previously_invalidated_locations: &[AreaResults],
-    ) -> bool {
-        // Check to see if it has already been Invalidated
-        for prev_loc in previously_invalidated_locations {
-            if self.consumes(prev_loc) {
-                println!("Invalidated because we have seen before");
-                // We already proved this one doesn't work, stop looking
-                return false;
-            }
-        }
-        for y in self.tile0.y.min(self.tile1.y)..=self.tile0.y.max(self.tile1.y) {
-            for x in self.tile0.x.min(self.tile1.x)..=self.tile0.x.max(self.tile1.x) {
-                if board[(y, x)] == 0 {
-                    return false;
-                }
-            }
-        }
-        true
+
+/// A different way to represent the board
+#[derive(Debug, Clone)]
+struct Board {
+    hull: Polygon<f32>,
+}
+impl Board {
+    pub fn new(red_tiles: &[Location]) -> Self {
+        let coords: Vec<Coord<f32>> = red_tiles
+            .iter()
+            .map(|tile| Coord {
+                x: tile.x as f32,
+                y: tile.y as f32,
+            })
+            .collect();
+        let mut linestring = LineString::from(coords);
+        linestring.close(); // Make sure that it is closed
+        let polygon = Polygon::new(linestring, Vec::new());
+        Self { hull: polygon }
     }
 
-    /// Determine if this area consumes the other area
-    ///
-    /// In order to determine this, we need to determine which is top right and which is bottom
-    /// left for both tiles. Then, we need to see if our bottom left is less than their bottom left
-    /// and our top right is greater than their top right
-    pub fn consumes(&self, other: &AreaResults) -> bool {
-        let (bottom_left, top_right) = self.tile0.get_corners(&self.tile1);
-        let (other_bottom_left, other_top_right) = other.tile0.get_corners(&other.tile1);
-        println!(
-            "Bottom lefts: {bottom_left:?} -- {other_bottom_left:?} -- {:?}\tTop rights: {top_right:?} -- {other_top_right:?} -- {:?}",
-            bottom_left < other_bottom_left,
-            top_right > other_top_right
-        );
-        bottom_left < other_bottom_left && top_right > other_top_right
+    pub fn contains(&self, result: &AreaResults) -> bool {
+        let min_x = result.tile0.x.min(result.tile1.x) as f32;
+        let max_x = result.tile0.x.max(result.tile1.x) as f32;
+        let min_y = result.tile0.y.min(result.tile1.y) as f32;
+        let max_y = result.tile0.y.max(result.tile1.y) as f32;
+        let rect: Polygon<_> =
+            Rect::new(Coord { x: min_x, y: min_y }, Coord { x: max_x, y: max_y }).into();
+        self.hull.contains(&rect)
     }
 }
 
