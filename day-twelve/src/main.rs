48d0d25fd71d21c2f6be03de4cafd6994175d99a
Matt Kagie <mjkagie@gmail.com>
2025-12-22 17:37:48 -0800

    Use heuristic instead

--- day-twelve/src/main.rs
+++ day-twelve/src/main.rs
@@ -140,6 +140,10 @@ impl Region {
             shape_counts,
         }
     }
+
+    pub fn area(&self) -> usize {
+        self.width * self.height
+    }
 }
 
 /// Parsing State
@@ -196,159 +200,25 @@ impl Driver {
         Self { shapes, regions }
     }
 
-    fn fits(grid: &[Vec<bool>], shape: &Shape, x: usize, y: usize) -> bool {
-        let h = shape.height();
-        let w = shape.width();
-        for dy in 0..h {
-            for dx in 0..w {
-                if shape.grid[dy][dx] && grid[y + dy][x + dx] {
-                    return false;
-                }
-            }
-        }
-        true
-    }
-
-    fn place(grid: &mut [Vec<bool>], shape: &Shape, x: usize, y: usize, value: bool) {
-        let h = shape.height();
-        let w = shape.width();
-        for dy in 0..h {
-            for dx in 0..w {
-                if shape.grid[dy][dx] {
-                    grid[y + dy][x + dx] = value;
-                }
-            }
-        }
-    }
-
-    fn can_fit_recursive(
-        grid: &mut [Vec<bool>],
-        shapes: &[Shape],
-        shape_idx: usize,
-        failed_scenarios: &mut HashSet<Scenario>,
-        depth: usize,
-    ) -> CanFitResult {
-        if shape_idx >= shapes.len() {
-            return CanFitResult::True;
-        }
-
-        if depth >= 200 {
-            return CanFitResult::MaxDepthReached;
-        }
-
-        // Try every variant
-        let variants = shapes[shape_idx].all_variants();
-        for (variant_idx, variant) in variants.into_iter().enumerate() {
-            let h = variant.height();
-            let w = variant.width();
-
-            // Try all starting positions
-            for y in 0..=grid.len() - h {
-                for x in 0..=grid[0].len() - w {
-                    let scenario = Scenario {
-                        grid: grid.to_owned(),
-                        shape_idx,
-                        variant_idx,
-                        x,
-                        y,
-                    };
-                    if failed_scenarios.contains(&scenario) {
-                        continue;
-                    }
-                    if Self::fits(grid, &variant, x, y) {
-                        // Place in the grid
-                        Self::place(grid, &variant, x, y, true);
-
-                        // If this works, great -- reduce the shape counts of this index by one and
-                        // try again
-                        let result = Self::can_fit_recursive(
-                            grid,
-                            shapes,
-                            shape_idx + 1,
-                            failed_scenarios,
-                            depth + 1,
-                        );
-                        if matches!(result, CanFitResult::True) {
-                            return CanFitResult::True;
-                        }
-                        if matches!(result, CanFitResult::MaxDepthReached) {
-                            return CanFitResult::MaxDepthReached;
-                        }
-                        // Otherwise, it failed
-                        failed_scenarios.insert(scenario);
-
-                        // If it does not, backgrack
-                        // undo the increment
-                        Self::place(grid, &variant, x, y, false);
-                    }
-                }
-            }
-        }
-        CanFitResult::False
-    }
-
-    pub fn can_fit(region: &Region, shapes: &[Shape]) -> bool {
-        let mut grid = vec![vec![false; region.width]; region.height];
-        // Create a list of shapes independent of the counts
-        let mut relevant_shapes = Vec::new();
-        for (shape_idx, count) in region.shape_counts.iter().enumerate() {
-            for _ in 0..*count {
-                relevant_shapes.push(shapes[shape_idx].clone());
-            }
-        }
-        // I would like to place largest shapes first because they will be the most restrictive
-        relevant_shapes.sort_by_key(|shape| shape.size());
-        relevant_shapes.reverse();
-
-        let mut failed_scenarios = HashSet::new();
-
-        matches!(
-            Self::can_fit_recursive(&mut grid, &relevant_shapes, 0, &mut failed_scenarios, 0),
-            CanFitResult::True
-        )
-    }
-
     pub fn part_one(&self) -> usize {
-        let success_counter = Arc::new(AtomicUsize::new(0));
-        let regions_left = Arc::new(AtomicUsize::new(self.regions.len()));
-        let successes: usize = self
-            .regions
-            .par_iter()
-            .enumerate()
-            .map(move |(region_idx, region)| {
-                let can_fit = Self::can_fit(region, &self.shapes);
-                if can_fit {
-                    success_counter.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
-                }
-                regions_left.fetch_sub(1, std::sync::atomic::Ordering::Relaxed);
-                println!(
-                    "Finished region {region_idx} -- can fit: {can_fit} -- Total: {}\t{} left",
-                    success_counter.load(std::sync::atomic::Ordering::Relaxed),
-                    regions_left.load(std::sync::atomic::Ordering::Relaxed)
-                );
-                can_fit as usize
-            })
-            .sum();
-        successes
+        // Let's solve this heuristically instead
+        let mut n_successes = 0;
+        for region in &self.regions {
+            let min_size: f32 = region
+                .shape_counts
+                .iter()
+                .enumerate()
+                .map(|(shape_idx, count)| {
+                    let size = self.shapes[shape_idx].size();
+                    size * count
+                })
+                .sum::<usize>() as f32;
+            if min_size * 1.2 <= region.area() as f32 {
+                n_successes += 1;
+            }
+        }
+        n_successes
     }
-}
-
-/// Failed location -- need to prune
-#[derive(Debug, Clone, Hash, Eq, PartialEq)]
-struct Scenario {
-    grid: Vec<Vec<bool>>,
-    shape_idx: usize,
-    variant_idx: usize,
-    x: usize,
-    y: usize,
-}
-
-/// Max Depth Indicator
-#[derive(Debug, Clone)]
-enum CanFitResult {
-    True,
-    False,
-    MaxDepthReached,
 }
 
 fn part_one(s: &str) -> usize {
